6bc1268e09c43456bdf36971fa59067904134abc
kevin <kevinlompo12@gmail.com>
2022-12-17 22:04:40 +0100

    Final

--- src/Main.java
+++ src/Main.java
@@ -7,28 +7,22 @@ public class Main {
     public static void main(String[] args) {
         List<Vehicule> vehicules = new ArrayList<>();
 
-   /*     vehicules.add(new Vehicule("B", 100, 150));
-        vehicules.add(new Vehicule("X", 5, 10));
-        vehicules.add(new Vehicule("DODO", 2, 1540));
-        vehicules.add(new Vehicule("DODOMOBILE", 1234566, 145640));*/
-
-        /*   */
-
+        // Moteur
         for (int i = 0; i<3; i++){
             ajout(vehicules);
-            Vehicule voitureChre = voiturePlusChere(vehicules);
-            System.out.println("\nLa voiture la plus chère est " + voitureChre.getMarque());
+
+            Vehicule voitureChere = voiturePlusChere(vehicules);
+            System.out.println("\nLa voiture la plus chère est " + voitureChere.getMarque());
 
             Vehicule voiturePetitK = voiturePlusPetitKilometrage(vehicules);
-
             System.out.println("La voiture ayant le plus petit kilometrage est " + voiturePetitK.getKilometrage() + " pour la voiture : " + voiturePetitK.getMarque());
 
             int prixTotal = prixTotal(vehicules);
             System.out.println("Le prix total est : " + prixTotal + "\n");
         }
-
     }
 
+    // Ajout d'un véhicule à la concession
     private static void ajout(List<Vehicule> vehicules) {
         System.out.print("Entrer la marque de la voiture(que des lettres) : ");
         String marque = saisieMarque();
@@ -42,6 +36,7 @@ public class Main {
         System.out.println(v.toString());
     }
 
+    // Saisie de la Marque du véhicule
     private static String saisieMarque() {
         Scanner sc = new Scanner(in);
         String saisi = "";
@@ -57,6 +52,7 @@ public class Main {
         return saisi;
     }
 
+    // Saisie du Kilometrage du véhicule
     private static int saisieKilometrage() {
         Scanner sc = new Scanner(in);
         int saisi = 0;
@@ -69,6 +65,7 @@ public class Main {
         return saisi;
     }
 
+    // Saisie du Prix du véhicule
     private static int saisiePrix() {
         Scanner sc = new Scanner(in);
         int saisi = 0;
@@ -81,24 +78,21 @@ public class Main {
         return saisi;
     }
 
+    // Retourne la voiture la plus chère
     private static Vehicule voiturePlusChere(List<Vehicule> vehicules) {
         Vehicule v = new Vehicule("",0,0);
         int val = Integer.MIN_VALUE;
-        List<Integer> test = new ArrayList();
+        List<Integer> test = new ArrayList<>();
         if (vehicules.size() > 1) {
             for (int i = 0; i < vehicules.size(); i++) {
                 test.add(vehicules.get(i).getPrix());
                 if (test.get(i) > val) val = test.get(i);
-
             }
-
             for (int i = 0; i < test.size(); i++) {
                 if (vehicules.get(i).getPrix() == val) {
                     v = vehicules.get(i);
                 }
-
             }
-
         } else if (vehicules.size() == 1) {
             v = vehicules.get(0);
         }
@@ -108,34 +102,31 @@ public class Main {
         return v;
     }
 
+    // Retourne la voiture ayant le plus petit kilometrage
     private static Vehicule voiturePlusPetitKilometrage(List<Vehicule> vehicules) {
         Vehicule v = new Vehicule("",0,0);
         int val = Integer.MAX_VALUE;
-        List<Integer> test = new ArrayList();
+        List<Integer> test = new ArrayList<>();
         if (vehicules.size() > 1) {
             for (int i = 0; i < vehicules.size(); i++) {
                 test.add(vehicules.get(i).getKilometrage());
                 if (test.get(i) < val) val = test.get(i);
-
             }
-
             for (int i = 0; i < test.size(); i++) {
                 if (vehicules.get(i).getKilometrage() == val) {
                     v = vehicules.get(i);
                 }
-
             }
-
         } else if (vehicules.size() == 1) {
             v = vehicules.get(0);
         }
         else {
             System.out.println(Constantes.aucuneDonnee);
         }
-
         return v;
     }
 
+    // Retourne le prix Total des véhicules de la concession
     private static int prixTotal(List<Vehicule> vehicules) {
         int total = 0;
         for (Vehicule v :
